ba41382e03e7d875d4181ea91483134bfb6a36a6
192211241 <122424318+Abinaya2311@users.noreply.github.com>
2023-03-24 12:50:05 +0530

    Add files via upload

---
+++ Day 5 - ANALYTICAL/D5A13.c
@@ -0,0 +1,13 @@
+main()
+{
+int n, i=1,sum=0;
+printf("Enter the values for n:");
+scanf("%d", &n);
+while(i<=n)
+{
+sum=sum+i;
+i++;
+}
+printf("The sum of n numbers is %d",sum);
+}
+
---
+++ Day 5 - ANALYTICAL/D5A15.c
@@ -0,0 +1,14 @@
+main()
+{
+int i=1,sum=0;
+clrscr();
+printf("Enter the values for n:");
+scanf("%d",n);
+while(i<=n)
+{
+sum==sum+i;
+i++;
+}
+printf("The sum of n numbers is",sum);
+}
+
